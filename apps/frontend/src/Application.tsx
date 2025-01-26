b51c99c2e6a952ae4b130ead0d1c1e485c200970
Claire Chabas <claire.chabas@gmail.com>
2025-01-26 21:39:02 +0100

    testing types check works

--- apps/frontend/src/Application.tsx
+++ apps/frontend/src/Application.tsx
@@ -1,19 +1,19 @@
 import * as React from 'react';
 import './App.css';
-import { Button } from '@base/ui/button';
+// import { Button } from '@base/ui/button';
 
 export default function Application() {
-  const [count, setCount] = React.useState(0);
+    const [count, setCount] = React.useState(0);
 
-  return (
-    <>
-      <h1>Welcome</h1>
-      <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>Increment</Button>
-        <p>
-          Count: <code>{count}</code>
-        </p>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <h1>Welcome</h1>
+            <div className="card">
+                <Button onClick={() => setCount((count) => count + 1)}>Increment</Button>
+                <p>
+                    Count: <code>{count}</code>
+                </p>
+            </div>
+        </>
+    );
 }
