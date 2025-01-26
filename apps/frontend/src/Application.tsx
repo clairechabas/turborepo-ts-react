46359744308b7c0da95ed606fadd37f39ad30a39
Claire Chabas <claire.chabas@gmail.com>
2025-01-26 23:03:37 +0100

    fix fake ts issue

--- apps/frontend/src/Application.tsx
+++ apps/frontend/src/Application.tsx
@@ -4,7 +4,6 @@ import { Button } from '@base/ui/button';
 
 export default function Application() {
     const [count, setCount] = React.useState(0);
-    console.log(button);
 
     return (
         <>
